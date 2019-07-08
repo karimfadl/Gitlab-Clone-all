11e2dcca14d8df8e6aa86b3b3a42ce2d7bc9b1b0
Karim Fadl <kareemfadl10@gmail.com>
2019-07-08 13:53:41 +0200

    Download Archived and Non Archived Repos

--- index.js
+++ index.js
@@ -9,6 +9,46 @@ const token = tokenJson.token;
 let Promise = require('bluebird')
 let cmd = require('node-cmd')
 
+//Download Non Archived Repos
+rp.get('https://www.gitlab.com/api/v4/groups\?per_page\=999', {
+  json: true,
+  qs: {
+    simple: true,
+  },
+  headers: {
+    'PRIVATE-TOKEN': token
+  }
+}).then(groups => {
+  let gids = _.map(groups, 'id')
+  let pgits = [];
+  let promises = [];
+  for (let gid of gids) {
+    promises.push(
+      rp.get(`https://www.gitlab.com/api/v4/groups/${gid}/projects\?per_page\=999`, {
+        json: true,
+        qs: {
+          simple: true,
+        },
+        headers: {
+          'PRIVATE-TOKEN': token
+        }
+      }).then(projects => {
+        let ps = _.map(projects, 'http_url_to_repo')
+        for (let p of ps) {
+          pgits.push(p);
+        }
+      })
+    )
+  }
+  Promise.all(promises).then(() => {
+    console.log(pgits);
+    for (let git of pgits) {
+      cmd.run(`git clone ${git} backup/${git.substring(19,git.length-4)}`);
+    }
+  });
+})
+
+//Download Archived Repos
 rp.get('https://www.gitlab.com/api/v4/groups\?archived\=true&per_page=100', {
   json: true,
   qs: {
